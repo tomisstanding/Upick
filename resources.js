abb2e1b7bc9a9c1d17fee258592e73aa690dad8d
Thomas Standing <tomcoap@gmail.com>
2017-04-05 09:03:56 -0400

    fix error

--- resources.js
+++ resources.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.use('/events', require('./controllers/events/index.js'));
 
-router.use('/test', (req, res) => res.jspon({message: 'success'}));
+router.use('/test', (req, res) => res.json({message: 'success'}));
 
 module.exports = router;
