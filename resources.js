05c4ed0bc5805f633bc93be6e0378b495e0c5876
Thomas Standing <tomcoap@gmail.com>
2017-04-05 09:01:42 -0400

    testing heroku backend

--- resources.js
+++ resources.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.use('/events', require('./controllers/events/index.js'));
 
+router.use('/test', (req, res) => res.jspon({message: 'success'}));
+
 module.exports = router;
